752073d53a4fc7393f3f545f6b4f8a729ab274fa
손소독 <vot768@naver.com>
2022-03-27 02:15:24 +0900

    1. 임베드에 제작자 관련 내용 추가

--- index.js
+++ index.js
@@ -12,6 +12,24 @@ client.on('message', msg => {
     if(msg.content === 'ping') {
         msg.reply('pong');
     }
+
+    if(msg.content === '제작자') {
+        const embed = new Discord.MessageEmbed()
+        .setAuthor("vot768", "https://blogpfthumb-phinf.pstatic.net/MjAyMTA2MTJfMjYg/MDAxNjIzNDY1OTMyNjI5.DNP5DcjwEZyBn9MH0Yo8-wUbZtOlH98xm1dpY5LKq7cg._7dRl2lQhL7d6AU4N1PctNGJ8nQGWMZ0DSCzgoQtEOwg.PNG.vot768/389_20210612114315.png?type=w161")
+        .setTitle("제작자 블로그")
+        .setURL("https://blog.naver.com/vot768")
+        .setDescription("제작자의 개인 블로그 입니다.")
+        .setThumbnail("https://toppng.com/uploads/preview/discordbot-bot-discord-11563261320iwm1tpnosh.png")
+        .addFields(
+            { name: "특기", value: "없음", inline: true },
+            { name: "기술", value: "없음", inline: true },
+            { name: "장점", value: "없음", inline: true },
+            { name: "단점", value: "없음", inline: true },
+        )
+        .setFooter("손소독");
+
+        msg.channel.send(embed);
+    }
 });
 
 client.login(process.env.DISCORD_TOKEN);
